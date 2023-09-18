30fc878430a30566da6602404d520cebde549d34
tarush10000 <tarushagarwal2003@gmail.com>
2023-09-19 00:07:46 +0530

    3D sexyyy

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -16,9 +17,40 @@ camera.position.setZ(30);
 
 renderer.render(scene, camera);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
-const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
+const geometry = new THREE.BoxGeometry( 30, 11, 0 );
+const texture = new THREE.TextureLoader().load('images/logo2.png'); 
+const material = new THREE.MeshStandardMaterial( { map: texture }); 
 const cube = new THREE.Mesh( geometry, material ); 
 scene.add( cube );
 
-renderer.render(scene, camera);
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.1, 24, 24);
+  const material = new THREE.MeshStandardMaterial( { color: 0xffffff });
+  const star = new THREE.Mesh( geometry, material );
+  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(50).fill().forEach(addStar);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate();
