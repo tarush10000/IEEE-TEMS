e1b05b2211ee7a347cfbfa141b3c73ecdd310fc5
tarush10000 <tarushagarwal2003@gmail.com>
2023-09-27 19:00:48 +0530

    new starting page

--- main.js
+++ main.js
@@ -4,28 +4,23 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import TWEEN from '@tweenjs/tween.js'
 import { PointLightHelper } from 'three';
-import { WebGLRenderer } from 'three';
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+  canvas: document.querySelector('#bg'), alpha: true
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+camera.position.setZ(5000);
+
+//transparent bg
+renderer.setClearColor( 0x000000, 0 ); // the default
 
 renderer.render(scene, camera);
-
-//background image
-const spaceTexture = new THREE.TextureLoader().load('images/jsbg.jpg');
-scene.background = spaceTexture;
-scene.backgroundIntensity = 0.1;
-scene.backgroundBlurriness = 0.99;
-
 //IEEE TEMS logo ka dabba
 const geometry = new THREE.BoxGeometry( 25, 11, 0 );
 const texture = new THREE.TextureLoader().load('images/logo.png'); 
@@ -57,17 +52,33 @@ scene.add(pointLight);
 //camera rotation with mouse
 const controls = new OrbitControls(camera, renderer.domElement);
 
+function animateCameraPosition() {
+  const initialPosition = {z : 5000 };
+  const targetPosition = {z : 30};
+  const duration = 2000; // Animation duration in milliseconds
+
+  new TWEEN.Tween(initialPosition)
+    .to(targetPosition, duration)
+    .easing(TWEEN.Easing.Quadratic.InOut) // Use bounce effect
+    .onUpdate(() => {
+      camera.position.setZ(initialPosition.z);
+    })
+    .start();
+}
+
+// Call the camera position animation on startup
+animateCameraPosition();
 
 //random stars in background
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.1, 24, 24);
-  const material = new THREE.MeshStandardMaterial( { color: 0xffffff });
-  const star = new THREE.Mesh( geometry, material );
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-Array(50).fill().forEach(addStar);
+// function addStar() {
+//   const geometry = new THREE.SphereGeometry(0.1, 24, 24);
+//   const material = new THREE.MeshStandardMaterial( { color: 0xffffff });
+//   const star = new THREE.Mesh( geometry, material );
+//   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+//   star.position.set(x, y, z);
+//   scene.add(star);
+// }
+// Array(50).fill().forEach(addStar);
 
 
 
@@ -81,13 +92,13 @@ scene.add(rotatorstar);
 
 const rotator2 = new THREE.SphereGeometry(0, 24, 24);
 const rotatormaterial2 = new THREE.MeshStandardMaterial( { color: 0xffffff });
-const rotatorstar2 = new THREE.Mesh( rotator, rotatormaterial );
+const rotatorstar2 = new THREE.Mesh( rotator2, rotatormaterial2 );
 rotatorstar2.position.set(0, 0, 0);
 scene.add(rotatorstar2);
 
 const rotator3 = new THREE.SphereGeometry(0, 24, 24);
 const rotatormaterial3 = new THREE.MeshStandardMaterial( { color: 0xffffff });
-const rotatorstar3 = new THREE.Mesh( rotator, rotatormaterial );
+const rotatorstar3 = new THREE.Mesh( rotator3, rotatormaterial3 );
 rotatorstar3.position.set(0, 0, 0);
 scene.add(rotatorstar3);
 
@@ -112,18 +123,15 @@ const rotatingSpheres = [];
 function rotatingelements(){
   
   const webloader = new THREE.TextureLoader();
-  const webtexture = webloader.load('images/coding.png');
-  const webgeometry = new THREE.BoxGeometry(0.3,0.3, 0.02);
+  const webgeometry = new THREE.SphereGeometry(0.1,24,24);
   const webmaterial = new THREE.MeshBasicMaterial({color: 0x1111ff});
   const web = new THREE.Mesh(webgeometry, webmaterial);
   web.position.x = 10;
   web.position.y = -1;
   rotatorstar.add(web);
 
-  
   const machineloader = new THREE.TextureLoader();
-  const machinetexture = machineloader.load('images/coding.png');
-  const machinegeometry = new THREE.BoxGeometry(0.3,0.3, 0.02);
+  const machinegeometry = new THREE.SphereGeometry(0.1,24,24);
   const machinematerial = new THREE.MeshBasicMaterial({color: 0xf111ff});
   const machine = new THREE.Mesh(machinegeometry, machinematerial);
   machine.position.x = 8;
@@ -131,8 +139,7 @@ function rotatingelements(){
   rotatorstar3.add(machine);
 
   const iotloader = new THREE.TextureLoader();
-  const iottexture = iotloader.load('images/coding.png');
-  const iotgeometry = new THREE.BoxGeometry(0.3,0.3, 0.02);
+  const iotgeometry = new THREE.SphereGeometry(0.1,24,24);
   const iotmaterial = new THREE.MeshBasicMaterial({color: 0xf1113f});
   const iot = new THREE.Mesh(iotgeometry, iotmaterial);
   iot.position.x = 6;
@@ -140,8 +147,7 @@ function rotatingelements(){
   rotatorstar2.add(iot);
 
   const devloader = new THREE.TextureLoader();
-  const devtexture = devloader.load('images/coding.png');
-  const devgeometry = new THREE.BoxGeometry(0.3,0.3, 0.02);
+  const devgeometry = new THREE.SphereGeometry(0.1,24,24);
   const devmaterial = new THREE.MeshBasicMaterial({color: 0x731139});
   const dev = new THREE.Mesh(devgeometry, devmaterial);
   dev.position.x = 4;
@@ -149,8 +155,7 @@ function rotatingelements(){
   rotatorstar.add(dev);
 
   const blogloader = new THREE.TextureLoader();
-  const blogtexture = blogloader.load('images/coding.png');
-  const bloggeometry = new THREE.BoxGeometry(0.3,0.3, 0.02);
+  const bloggeometry = new THREE.SphereGeometry(0.1,24,24);
   const blogmaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
   const blog = new THREE.Mesh(bloggeometry, blogmaterial);
   blog.position.x = 2;
@@ -158,8 +163,7 @@ function rotatingelements(){
   rotatorstar2.add(blog);
 
   const designloader = new THREE.TextureLoader();
-  const designtexture = designloader.load('images/coding.png');
-  const designgeometry = new THREE.BoxGeometry(0.3,0.3, 0.02);
+  const designgeometry = new THREE.SphereGeometry(0.1,24,24);
   const designmaterial = new THREE.MeshBasicMaterial({color: 0x2eeeef});
   const design = new THREE.Mesh(designgeometry, designmaterial);
   design.position.x = 0;
@@ -203,13 +207,11 @@ function onMouseMove(event) {
         .to({ x: 10, y: 10, z: 10 }, 100)
         .easing(TWEEN.Easing.Quadratic.Out)
         .start();
-        // ambientLight.intensity = 1;
     } else {
       new TWEEN.Tween(sphere.scale)
         .to({ x: 0, y: 0, z: 0 }, 100) 
         .easing(TWEEN.Easing.Quadratic.Out) 
         .start();
-        // ambientLight.intensity = 0.2;
     }
   });
 }
@@ -221,9 +223,3 @@ function animateTween() {
 }
 
 animateTween();
-
-
-
-
-
-const about = document.getElementById('about');
